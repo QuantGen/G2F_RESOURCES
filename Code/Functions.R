b58b5b3e0354fa06dc5f21d6b6dd909c266b6982
Fernando Aguate <fmaguate@gmail.com>
2021-01-18 10:52:24 -0500

    Update Functions.R

--- Code/Functions.R
+++ Code/Functions.R
@@ -1,1 +1,16 @@
+# Function to rename columns
+
+rename_columns <- function(existing_colname, new_colname, mydata = pData){
+  lapply(mydata, function(x){
+    x_col <- grep(existing_colname, colnames(x), ignore.case=T)
+    if(is.na(new_colname) & length(x_col) == 1) { x[,x_col] <- NULL } else {
+      if(length(x_col) == 1) colnames(x)[x_col] <- new_colname }
+    if(length(x_col) > 1) stop(paste('Two columns matched existing_colname:', paste(colnames(x)[x_col], collapse = ' and ')))
+    if(length(x_col) < 1) warning('no column matched existing_colname in at least one dataset.')
+    return(x)
+  })
+}
+
+#####
+
 
