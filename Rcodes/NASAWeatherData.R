4ecaf321ab63aa190318f3690aa6ddae59ac02c0
Fernando Aguate <fmaguate@gmail.com>
2021-05-18 12:17:40 -0400

    Update NASAWeatherData.R

--- Rcodes/NASAWeatherData.R
+++ Rcodes/NASAWeatherData.R
@@ -9,10 +9,10 @@ allpars <- do.call(rbind, lapply(parameters, function(x){
              unit = paste(x$SB_Units))
 }))
 
-# daily Agroclimatic variables
-AGvar <- allpars[allpars$AG & allpars$daily,]
-AGvar <- AGvar[rownames(AGvar) != 'WSC',] # Removing corrected wind speed because it's not available 
-AGvar
+# Variables of interest
+varInt <- c('PRECTOT','T2M','T2M_MIN','T2M_MAX','ALLSKY_SFC_SW_DWN','PS','RH2M','T2MDEW','T2MWET','WS2M','DNR')
+
+AGvar <- allpars[varInt,]
 
 # Load location data
 info_loc <- read.csv('Data/OutputFiles/info_loc.csv')
@@ -22,48 +22,32 @@ info_loc$sowing <- as.Date(info_loc$sowing)
 info_loc$harvesting <- as.Date(info_loc$harvesting)
 
 # Get data for all locations
-AGdata <- list()
+data1 <- list()
 for (i in 1:nrow(info_loc)) {
-  AGdata[[i]] <- cbind(location = info_loc$Location[i],
-                       as.data.frame(get_power(community = "AG",
-                                               lonlat = c(info_loc$lon[i], info_loc$lat[i]),
-                                               pars = rownames(AGvar),
-                                               dates = c(info_loc$sowing[i], info_loc$harvesting[i]),
-                                               temporal_average = "DAILY")))
+  location = info_loc$Location[i]
+  tmp <- as.data.frame(get_power(community = 'AG',
+                                 lonlat = c(info_loc$lon[i], info_loc$lat[i]),
+                                 pars = rownames(AGvar)[AGvar$daily],
+                                 dates = c(info_loc$sowing[i], info_loc$harvesting[i]),
+                                 temporal_average = 'DAILY'))
+  tmp2 <- as.data.frame(t(get_power(community = 'AG',
+                                 lonlat = c(info_loc$lon[i], info_loc$lat[i]),
+                                 pars = rownames(AGvar)[!AGvar$daily],
+                                 dates = c(info_loc$sowing[i], info_loc$harvesting[i]),
+                                 temporal_average =  'CLIMATOLOGY')))
+  tmp3 <- tmp2[tmp$MM+3,,drop=F]
+  colnames(tmp3) <- tmp2['PARAMETER',]
+  
+  data1[[i]] <- cbind(location,tmp,tmp3)  
   message(i, ' loc = ', info_loc$Location[i], ' done.')
 }
-AGdata <- do.call(rbind, AGdata)
+data2 <- do.call(rbind, data1)
 
-# Format colnames
-AGdata <- AGdata[,c('YYYYMMDD', 'location', 'PRECTOT', 'T2M', 'T2M_MIN', 'T2M_MAX', 'ALLSKY_SFC_LW_DWN', 'ALLSKY_SFC_SW_DWN',
-                    'ALLSKY_TOA_SW_DWN', 'PS', 'RH2M', 'T2MDEW', 'T2MWET', 'TS', 'WS10M')]
-colnames(AGdata)[c(1, 3:6)] <- c('date', 'rainfall', 'temp', 'temp_min', 'temp_max')
-colnames(AGdata) <- casefold(colnames(AGdata))
-
-# Add evapotranspiration (eto) isomg ClimMobTools package
-install.packages('ClimMobTools')
-library(ClimMobTools)
-# More information about this calculation: http://www.fao.org/3/s2022e/s2022e07.htm
-
-res <- list()
-for (i in 1:nrow(info_loc)) {
-  # growth period
-  dates <- seq(info_loc[i, 'sowing'], info_loc[i, 'harvesting'], 1)
-  gp_Kc <- c(0.4, 0.8, 1.15, 0.7) # Kc for Maize, grain from Table 8
-  gp_proportion <- cumsum(c(30/180, 50/180, 60/180, 40/180)) # Proportion of growth stages based on Table 7
-  gp_starting <- as.Date(quantile(as.numeric(dates), probs = c(0, gp_proportion)), origin = '1970-01-01')
-  gp_stages <- as.numeric(cut(dates, breaks = gp_starting, right = T, include.lowest = T))
-  gp_eto <- gp_stages
-  for (st in 1:4) {
-    eto <- as.numeric(ETo(info_loc[i, c('lon', 'lat')],
-                          day.one = gp_starting[st],
-                          span = sum(gp_stages == st), Kc = gp_Kc[st]))
-    gp_eto[gp_stages == st] <- eto
-  }
-  res[[i]] <- data.frame(location = info_loc$Location[i], date = dates, eto = gp_eto)
-}
-res2 <- do.call(rbind, res)
-AGdata <- merge(AGdata, res2, by = c('date', 'location'))
+# Format row and col names
+rownames(data2) <- NULL
+data2 <- data2[,c(8,1,9:ncol(data2))]
+colnames(data2)[c(1, 3:6)] <- c('date', 'rainfall', 'temp', 'temp_min', 'temp_max')
+colnames(data2) <- casefold(colnames(data2))
 
 # Save data
-write.csv(AGdata, file = 'Data/OutputFiles/NASAdaily.csv', quote = F, row.names = F)
+write.csv(data2, file = 'Data/OutputFiles/NASAdaily.csv', quote = F, row.names = F)
