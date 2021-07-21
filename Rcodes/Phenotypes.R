ee13ae067b926c2be69e600be7e1124b99463851
Fernando Aguate <fmaguate@gmail.com>
2021-07-21 14:13:13 -0400

    Update Phenotypes.R

--- Rcodes/Phenotypes.R
+++ Rcodes/Phenotypes.R
@@ -1,5 +1,5 @@
 # Files that contain phenotype data
-files <- c("data_phenotypes_2017.csv", "data_phenotypes_2018.csv", "data_phenotypes_2019.csv")
+files <- list.files('Data/Phenotypes/')
 
 # Read files
 pData <- list()
@@ -11,53 +11,28 @@ pData <- lapply(pData, function(x){colnames(x) <- tolower(colnames(x));x})
 # Load function to rename columns
 source('Tools/Functions.R')
 
-# Renaming columns
-rep_matrix <- matrix(c('(field.*location)', 'location',
-                     'RecId', NA,
-                     'replicate', 'rep',
-                     '(local.*check)', 'local_check',
-                     '(Plot.*Length.*Field)', 'plot_length',
-                     '(Alley.*Length)', 'alley_length',
-                     '(Row.*Spacing)', 'row_spacing',
-                     '(Plot.*Area)', 'plot_area',
-                     '(Rows.*Plot)', 'rows',
-                     'block', NA,
-                     '(Packet.*Plot)', NA,
-                     '(Kernels.*Packet)', NA,
-                     'Seed', 'seed_number',
-                     '(Date.*Planted)', 'date_plant',
-                     '(Date.*Harvest)', 'date_harvest',
-                     'Anthesis', 'date_anthesis',
-                     '(Silking.*date)', 'date_silking',
-                     'Pollen', NA,
-                     '(Silk.*DAP)', NA,
-                     '(Plant.*Height)', 'height_plant',
-                     '(Ear.*Height)', 'height_plant',
-                     '(Stand.*plants)', 'plants_stand',
-                     '(Root.*Lodging)', 'plants_root_lodging',
-                     '(Stalk.*Lodging)', 'plants_stalk_lodging',
-                     '(grain.*moisture)', 'grain_moisture',
-                     '(Test.*Weight)', 'test_weight',
-                     '(Plot.*Weight)', 'plot_weight',
-                     '(Grain.*Yield)', 'yield',
-                     '(Plot.*Discarded)', 'discarded',
-                     'comments', NA,
-                     'additional', NA), ncol=2, byrow=T)
-# Ignore warnings
-for (i in 1:nrow(rep_matrix))
-  pData <- rename_columns(rep_matrix[i,1], rep_matrix[i,2])
+# Rename columns
+col_names <- data.frame(
+  old_names = c('year', '(field.*location)', 'replicate', '(plot.*area)', 'seed', '(date.*planted)', '(date.*harvest)',
+                'anthesis', '(silking.*date)', '(stand.*plants)', '(grain.*moisture)', '(grain.*yield)'),
+  new_names = c('year', 'location', 'rep', 'plot_area', 'seed_number', 'date_plant', 'date_harvest', 'date_anthesis',
+                'date_silking', 'plants_stand', 'grain_moisture', 'yield'))
+for (i in 1:nrow(col_names)) pData <- rename(col_names$old_names[i], col_names$new_names[i], pData)
 
 # merge datasets from different years
-myCols <- names(which(table(unlist(lapply(pData, colnames))) == 3))
-pheno <- do.call(rbind, lapply(pData, function(x) x[myCols]))
+pheno <- do.call(rbind, lapply(pData, function(x) x[,col_names$new_names]))
 
-# Remove empty and discarded rows
+# Remove empty rows
 pheno <- pheno[rowSums(is.na(pheno)) != ncol(pheno),]
-pheno <- pheno[!pheno$discarded %in% c('Yes', 'yes'),]
-pheno <- pheno[!pheno$location == 'GEH1',] # Remove Germany
-pheno$discarded <- NULL
+pheno <- pheno[!is.na(pheno$yield) & !pheno$yield == 'NA',]
+
+# Remove Germany
+pheno <- pheno[!pheno$location == 'GEH1',]
 
 # Rename locations
+pheno$location[pheno$location == 'IAH1a'] <- 'IAH1'
+pheno$location[pheno$location == 'IAH1b'] <- 'IAH1'
+pheno$location[pheno$location == 'IAH1c'] <- 'IAH1'
 pheno$location[pheno$location == 'NEH3'] <- 'NEH2'
 pheno$location[pheno$location == 'NEH4'] <- 'NEH2'
 pheno$location[pheno$location == 'TXH1-Dry'] <- 'TXH1'
@@ -67,12 +42,11 @@ pheno$location[pheno$location %in% c('TXH2', 'TXH3')] <- 'TXH1'
 pheno$location[pheno$location == 'ONH1'] <- 'ONH2'
 pheno$location[pheno$location == 'NEH1'] <- 'NEH2'
 
-# Change yield measure to kg/ha (ignore warning)
+# Change yield measure to kg/ha
 pheno$yield <- as.numeric(pheno$yield) * 62.77
 
 # Change date format
 for (i in grep('date', colnames(pheno)))
   pheno[,i] <- as.Date(strptime(pheno[,i], "%m/%d/%y"))
-
 
 write.csv(pheno, file = 'Data/OutputFiles/phenotypes.csv', quote = F, row.names = F)
